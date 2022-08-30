32aa3e16adf403b8040676460a1f5a9bdf0033f8
Ranjan Nagarkoti <79069789+RanjanNagarkoti@users.noreply.github.com>
2022-08-30 08:41:40 +0545

    Question 24 solved.

---
+++ Question24.java
@@ -0,0 +1,27 @@
+package matrixinternship;
+
+// @author Ranjan
+public class Question24 {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 1, 100, 98, 56, 32, 41, 74, 92, 21, 26, 36};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("The second highest number in array is " + arr[arr.length - 2]);
+    }
+
+}
