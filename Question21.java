a9cbe1d175495331a0efd9ba11575add04e60492
Ranjan Nagarkoti <79069789+RanjanNagarkoti@users.noreply.github.com>
2022-08-30 08:23:08 +0545

    Question 21 solved.

---
+++ Question21.java
@@ -0,0 +1,24 @@
+package matrixinternship;
+
+// @author Ranjan
+public class Question21 {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        int i, j, k = 1;
+
+        for (i = 1; i <= n; i++) {
+
+            for (j = 1; j <= i; j++) {
+
+                System.out.print(k);
+
+                k++;
+            }
+
+            System.out.println();
+        }
+    }
+
+}
